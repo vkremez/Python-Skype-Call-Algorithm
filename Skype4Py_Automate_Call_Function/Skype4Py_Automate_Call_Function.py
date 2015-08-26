82739f1743ca1ae7c97327af2498307f6320c527
vkremez <vkremez@hotmail.com>
2015-08-25 23:03:19 -0400

    Update Source Code

--- Skype4Py_Automate_Call_Function/Skype4Py_Automate_Call_Function.py
+++ Skype4Py_Automate_Call_Function/Skype4Py_Automate_Call_Function.py
@@ -1,72 +1,66 @@
-'''
-
-'''
-import sys
-import time
-import Skype4Py
+import sys, time, Skype4Py
 from Skype4Py import call
 
-num = raw_input("Please dial the phone number in the format of +708123125: ")
-pause = raw_input("Please enter the desired delay call time in seconds: ")
-
+num = raw_input("Please enter the desired phone number like +1234567890: ")
+pause = raw_input("Input pause(sec): ")
 while 1==1:
-    # The var will get value in OnCall handler
+    # This variable will get its actual value in OnCall handler
     CallStatus = 0
-
-CallIsFinished = set([Skype4Py.clsFailed, Skype4Py.clsFinished, Skype4Py.clsMissed, Skype4Py.clsMissed, Skype4Py.clsRefused, Skype4Py.clsBusy, Skype4Py.clsCancelled]);
-
-def AttachmentStatusText(status):
-    return skype.Convert.AttachmentStatusToText(status)
-
-def CallStatusText(status):
-    return skype.Convert.CallStatusToText(status)
-
-# The below-referenced is fired up when the status of Call object has changed.
-def onCall(call, status):
-    global CallStatus
-    CallStatus = status
-    print "Call status: " + CallStatusText(status)
-    if CallStatusText(status) == "Call In Progress":
-        call.Finish()
-        print "Time To Pause..."
-
-# This handler is fired up when Skype attachment status changes
-def OnAttach(status):
-    print "API Attachment Status: " + AttachmentStatusText(status)
-    if status == Skype4Py.apiAttachAvailable:
-        skype.Attach()
-
-try:
-    Cmdline = num
-except:
-    print "Phone Number Error!"
-    sys.exit()
-
-# Create a Skype object and assign event handlers.
-skype = Skype4Py.Skype()
-skype.onAttachmentStatus = OnAttach
-skype.OnCallStatus = onCall
-
-# Start Skype
-if not skype.Client.IsRunning:
-    print "Starting Skype..."
-    skype.Client.Start()
-
-# Attaching to Skype...
-print "Connecting to Skype..."
-skype.Attach()
-skype.PlaceCall(Cmdline)
-
-# Checking our contact list to see if there is a contact already there.
-Found = False
-
-# Loop untill CallStatus get "call terminated" values in OnCall handler
-while not CallStatus in CallIsFinished:
-    pass
-
-time.sleep(int(pause))
-
-
-
-
+    
+    # Here we define a set of call statuses that indicate a call has been either aborted or finished
+    CallIsFinished = set ([Skype4Py.clsFailed, Skype4Py.clsFinished, Skype4Py.clsMissed, Skype4Py.clsRefused, Skype4Py.clsBusy, Skype4Py.clsCancelled]);
+    
+    def AttachmentStatusText(status):
+       return skype.Convert.AttachmentStatusToText(status)
+    
+    def CallStatusText(status):
+        return skype.Convert.CallStatusToText(status)
+    
+    # This handler is fired when status of Call object has changed
+    def OnCall(call, status):
+        global CallStatus
+        CallStatus = status
+        print 'Call status: ' + CallStatusText(status)
+        if CallStatusText(status)=='Call in Progress': #Call in progress
+            call.Finish()
+            print "Waiting pause..."
+    
+    
+    # This handler is fired when Skype attatchment status changes
+    def OnAttach(status):
+        print 'API attachment status: ' + AttachmentStatusText(status)
+        if status == Skype4Py.apiAttachAvailable:
+            skype.Attach()
+    
+    # Let's see if we were started with a command line parameter..
+    try:
+        CmdLine = num
+    except:
+        print 'Missing command line parameter'
+        sys.exit()
+    
+    # Creating Skype object and assigning event handlers..
+    skype = Skype4Py.Skype()
+    skype.OnAttachmentStatus = OnAttach
+    skype.OnCallStatus = OnCall
+    
+    # Starting Skype if it's not running already..
+    if not skype.Client.IsRunning:
+        print 'Starting Skype..'
+        skype.Client.Start()
+    
+    # Attatching to Skype..
+    print 'Connecting to Skype..'
+    skype.Attach()
+    skype.PlaceCall(CmdLine)
+    
+    # Checking if what we got from command line parameter is present in our contact list
+    Found = False
+    
+    
+    # Loop until CallStatus gets one of "call terminated" values in OnCall handler
+    while not CallStatus in CallIsFinished:
+        pass
+    
+    time.sleep(int(pause))
 
